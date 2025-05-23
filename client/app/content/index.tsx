49d12be46fca5253839231f27cd71bb150d8c5e4
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2025-05-23 09:35:34 +0530

    studing file based routing in expo

---
+++ client/app/content/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Content() {
+  return (
+    <View
+      className=" flex-1 items-center justify-center "
+    >
+      <Text className=" text-white">Jai shree ram , mera naam manoj singh rawat hai.</Text>
+    </View>
+  );
+}
--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -6,6 +7,9 @@ export default function Index() {
       className=" flex-1 items-center justify-center bg-white text-black"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/content">
+        <Text className="text-blue-500">Go to content</Text>
+      </Link>
     </View>
   );
 }
